9bf533e02e956da88abc238062d57a103ea4e0b4
Komal Dhok <71183193+komal-1010@users.noreply.github.com>
2025-11-29 08:18:27 +0000

    added auth

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import { RunnableSequence } from '@langchain/core/runnables'
 import multer from 'multer'
 import fs from 'fs/promises';
 import PDFParser from 'pdf2json'
-import {v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid"
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 const app = express();
@@ -22,7 +22,7 @@ console.log("connection established")
 const db = client.db('ai')
 const collection = db.collection('documents')
 
-const usercollection=db.collection('users')
+const usercollection = db.collection('users')
 const embedder = new OpenAIEmbeddings({
     apiKey: process.env.OPENROUTER_API_KEY,
     configuration: {
@@ -57,11 +57,11 @@ Answer clearly:`]
 //RAG Chain
 const ragChain = new RunnableSequence([
     async (input) => {
-        const userId=input.userId
+        const userId = input.userId
         await client.connect()
         const embedding = await embedder.embedQuery(input.question)
         const docs = await collection.aggregate([
-             { $match: { userId } },
+            { $match: { userId } },
             {
                 $vectorSearch: {
                     index: 'default',
@@ -82,60 +82,72 @@ const ragChain = new RunnableSequence([
     model
 ]);
 
-app.post('/register',async(req,res)=>{
-    try{
-        const {email,password}=req.body;
-        if(!email||!password){
-            return res.status(400).json({error:"Email and Password is required"})
+app.post('/register', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: "Email and Password is required" })
         }
-        const existing=await usercollection.findOne({email})
-        if(existing) return res.status(400).json({error:"Email already exist"})
-        const hasedPassword=await bcrypt.hash(password,10);
-        const newuser={
-            email,password:hasedPassword,createdAt:new Date()
+        const existing = await usercollection.findOne({ email })
+        if (existing) return res.status(400).json({ error: "Email already exist" })
+        const hasedPassword = await bcrypt.hash(password, 10);
+        const newuser = {
+            email, password: hasedPassword, createdAt: new Date()
         }
-        const result=await usercollection.insertOne(newuser)
-        return res.status(201).json({"message":"User registed sucessfully",userId:result.insertedId})
-    }catch(err){
-        console.error("register error:",err)
-        res.status(500).json("error",'server error')
+        const result = await usercollection.insertOne(newuser)
+        return res.status(201).json({ "message": "User registed sucessfully", userId: result.insertedId })
+    } catch (err) {
+        console.error("register error:", err)
+        res.status(500).json("error", 'server error')
     }
 })
-app.post('/login',(req,res)=>{
-    try{
-        const {email,password}=req.body;
-        if(!email||!password){
-            return res.status(400).json({error:"Email and Password is required"})
+app.post('/login', (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: "Email and Password is required" })
         }
-        const user=usercollection.findOne({email})
-        if(!user){
-            return res.status(401).json({error:'Email and Password is invalid'})
+        const user = usercollection.findOne({ email })
+        if (!user) {
+            return res.status(401).json({ error: 'Email and Password is invalid' })
         }
-        const isValid=bcrypt.compare(password,user.password)
-        if(!isValid){
-            return res.status(401).json({error:'Invalid password or email'})
+        const isValid = bcrypt.compare(password, user.password)
+        if (!isValid) {
+            return res.status(401).json({ error: 'Invalid password or email' })
         }
         //JWT payload
-        const token=jwt.sign(
-            {sub:user._id.toString(),email:user.email},
+        const token = jwt.sign(
+            { sub: user._id.toString(), email: user.email },
             process.env.JWT_SECRET,
-            {expiresIn:"7d"}
+            { expiresIn: "7d" }
         )
         return res.status(200).json({
-            message:"Login Successful",
+            message: "Login Successful",
             token,
-            user:{email:user.email,id:user._id}
+            user: { email: user.email, id: user._id }
         })
-    }catch(err){
-        console.error("Login error",err)
+    } catch (err) {
+        console.error("Login error", err)
         res.status(500).json("server error")
     }
 })
-//API endpoint
-app.post('/ask',async (req, res) => {
+
+const auth = (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ error: "No token provided" })
     try {
-        const { question, chatId} = req.body
-        const userId=req.user.id
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+        req.user={id:decoded.sub}
+        next();
+    }catch(err){
+        return res.status(401).json({error:'Invalid token'})
+    }
+}
+//API endpoint
+app.post('/ask', auth,async (req, res) => {
+    try {
+        const { question, chatId } = req.body
+        const userId = req.user.id
         if (!question) {
             return res.status(400).json({ error: 'question is required' })
         }
@@ -143,39 +155,39 @@ app.post('/ask',async (req, res) => {
         await db.collection("messages").insertOne({
             chatId,
             userId,
-            role:"user",
-            text:question,
-            createdAt:new Date()
+            role: "user",
+            text: question,
+            createdAt: new Date()
         })
 
-       //2.Run RAG
-       const history=await db.collection("messages")
-       .find({chatId})
-       .sort({createdAt:1})
-       .toArray()
+        //2.Run RAG
+        const history = await db.collection("messages")
+            .find({ chatId })
+            .sort({ createdAt: 1 })
+            .toArray()
 
-       const answer=await ragChain.invoke({
-        question,
-        history:history.map(m=>({sender:m.role,text:m.text})),
-        userId
-       })
-       //Save AI reply
-       await db.collection('chats').updateOne(
-        {_id:new ObjectId(chatId)},
-        {$set:{updatedAt:new Date()}}
-       )
-       res.status(201).json({"message":answer.content})
+        const answer = await ragChain.invoke({
+            question,
+            history: history.map(m => ({ sender: m.role, text: m.text })),
+            userId
+        })
+        //Save AI reply
+        await db.collection('chats').updateOne(
+            { _id: new ObjectId(chatId) },
+            { $set: { updatedAt: new Date() } }
+        )
+        res.status(201).json({ "message": answer.content })
     } catch (err) {
         console.error('Error:', err)
         res.status(500).json({ error: 'server error' })
     }
 })
 
-app.post('/upload', upload.single("file"), async (req, res) => {
+app.post('/upload', auth,upload.single("file"), async (req, res) => {
     try {
         const file = req.file;
-        const userId=req.user.id;
-        const docId=uuidv4()
+        const userId = req.user.id;
+        const docId = uuidv4()
         if (!file) return res.status(400).json({ "message": "No file uploaded" })
         let text = "";
 
@@ -223,7 +235,7 @@ app.post('/upload', upload.single("file"), async (req, res) => {
             await collection.insertOne({
                 userId,
                 docId,
-                filename:file.originalname,
+                filename: file.originalname,
                 text: chunk,
                 embedding: vector,
                 createdAt: new Date()
@@ -239,71 +251,71 @@ app.post('/upload', upload.single("file"), async (req, res) => {
 
 
 // Add Streaming to Your Express.js API
-app.post('/ask/stream',async(req,res)=>{
-    try{
-        const {question}=req.body;
-        res.setHeader("Content-Type","text/event-stream");
-        res.setHeader('Cache-Control','no-cache');
+app.post('/ask/stream',auth, async (req, res) => {
+    try {
+        const { question } = req.body;
+        res.setHeader("Content-Type", "text/event-stream");
+        res.setHeader('Cache-Control', 'no-cache');
         res.flushHeaders();
-        const stream=await model.stream(question);
-        for await(const chunk of stream){
-            res.write(chunk.concat??"")
+        const stream = await model.stream(question);
+        for await (const chunk of stream) {
+            res.write(chunk.concat ?? "")
         }
         res.end();
-    }catch(err){
-        console.error("Streaming error:",err);
-        res.status(500).json({error:"Streaming Failed"})
+    } catch (err) {
+        console.error("Streaming error:", err);
+        res.status(500).json({ error: "Streaming Failed" })
     }
 })
 
-app.post('/chat',async(req,res)=>{
-    const userId=req.user.id;
-    const {title}=req.body;
-    const chat={
+app.post('/chat', auth,async (req, res) => {
+    const userId = req.user.id;
+    const { title } = req.body;
+    const chat = {
         userId,
-        title:title||"New Chat",
-        createdAt:new Date(),
-        updatedAt:new Date()
+        title: title || "New Chat",
+        createdAt: new Date(),
+        updatedAt: new Date()
     }
-    const result=await db.collection("chats").insertOne(chat)
-    res.json({chatId:result.insertedId})
+    const result = await db.collection("chats").insertOne(chat)
+    res.json({ chatId: result.insertedId })
 })
-app.post('/chats/:chatId/messages',async(req,res)=>{
-    const {chatId}=req.params;
-    const userId=req.user.id;
-    const {role,text}=req.body;
+app.post('/chats/:chatId/messages',auth, async (req, res) => {
+    const { chatId } = req.params;
+    const userId = req.user.id;
+    const { role, text } = req.body;
     await db.collection('messages').insertOne({
         chatId,
         userId,
         role,
         text,
-        createdAt:new Date()
+        createdAt: new Date()
     })
     //update chat timestamp
     await db.collection('chats').updateOne(
-        {_id:new ObjectId(chatId)},
-        {$set:{updatedAt:new Date()}}
+        { _id: new ObjectId(chatId) },
+        { $set: { updatedAt: new Date() } }
     )
-    res.json({success:true})
+    res.json({ success: true })
 })
 //get all chat sessions
-app.get('/chats',async(req,res)=>{
-    const chats=await db.collection('chats').find({userId:req.user.id}).sort({updatedAt:-1}).toArray()
-    res.json({chats})
+app.get('/chats',auth, async (req, res) => {
+    const chats = await db.collection('chats').find({ userId: req.user.id }).sort({ updatedAt: -1 }).toArray()
+    res.json({ chats })
 })
 //load messages of chat 
-app.get("chats/:chatId/messages",async(req,res)=>{
-    const {chatId}=req.params;
-    const messages=await db.collection("messages").find({chatId}).sort({createdAt:-1}).toArray()
-    res.json({messages})
+app.get("/chats/:chatId/messages",auth, async (req, res) => {
+    const { chatId } = req.params;
+    const messages = await db.collection("messages").find({ chatId }).sort({ createdAt: -1 }).toArray()
+    res.json({ messages })
 })
 //delete the chats
-app.delete("chats/:chatId",async(req,res)=>{
-    const {chatId}=req.params;
-    const userId=req.user.id;
-    await db.collection("chats").deleteOne({_id:new ObjectId(chatId),userId})
-    await db.collection("messages").deleteMany({chatId})
-    res.json({success:true})
+app.delete("/chats/:chatId",auth, async (req, res) => {
+    const { chatId } = req.params;
+    const userId = req.user.id;
+    await db.collection("chats").deleteOne({ _id: new ObjectId(chatId), userId })
+    await db.collection("messages").deleteMany({ chatId })
+    res.json({ success: true })
 })
 app.listen(3000, () => {
     console.log('server running on port 3000')
